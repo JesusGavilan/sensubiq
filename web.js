8a1fea8fd61097bb39a84e431b357837eb5d9f36
Ubuntu <ubuntu@ip-172-31-33-150.eu-west-1.compute.internal>
2013-07-20 18:06:01 +0000

    First version of the index web of Sensubiq

---
+++ web.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var fs      = require('fs');
+var index   = fs.readFileSync('./index.html', 'utf8');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send(index.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
